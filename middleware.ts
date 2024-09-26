26f8947936b0032befe18b4970c103f62cc73acc
sj0724 <sj07245@naver.com>
2024-09-26 11:11:09 +0900

    feat: 미들웨어 설정, 로그인시 인증 페이지 접근 제한

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,28 @@
-export { auth as middleware } from '@/auth';
+/* eslint-disable consistent-return */
+import { NextResponse } from 'next/server';
+import { auth } from './auth';
+
+export const authRoutes = ['/sign-in', '/sign-up'];
+
+export default auth(async (req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+  const isApiAuthRoute = nextUrl.pathname.startsWith(`/api/auth`);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL('/', nextUrl));
+    }
+    return;
+  }
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
