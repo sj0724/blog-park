b908a7a7e1da442157e4a12969559f704e6be5ad
sj0724 <sj07245@naver.com>
2024-09-28 00:12:31 +0900

    style: nav 수정

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import PostContainer from './_components/post-container';
 
 export default function Home() {
   return (
-    <div className='flex w-screen'>
+    <div className='flex max-w-screen'>
       <PostContainer />
     </div>
   );
