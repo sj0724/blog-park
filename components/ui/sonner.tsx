a0f95298e6bc839081aefd879fb5586139770ac0
sj0724 <sj07245@naver.com>
2024-09-27 14:52:10 +0900

    feat: toast 세팅

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -5,13 +5,16 @@ import { Toaster as Sonner } from 'sonner';
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
-const Toaster = ({ ...props }: ToasterProps) => {
+function Toaster({ ...props }: ToasterProps) {
   const { theme = 'system' } = useTheme();
 
   return (
     <Sonner
       theme={theme as ToasterProps['theme']}
+      position='top-right'
       className='toaster group'
+      richColors
+      style={{ top: '96px' }}
       toastOptions={{
         classNames: {
           toast:
@@ -26,6 +29,6 @@ const Toaster = ({ ...props }: ToasterProps) => {
       {...props}
     />
   );
-};
+}
 
-export { Toaster };
+export default Toaster;
