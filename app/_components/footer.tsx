7e5a702940942e27189f9afb46807360aa30d3e9
sj0724 <sj07245@naver.com>
2024-09-28 11:09:07 +0900

    feat: footer 구현

---
+++ app/_components/footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer className='h-28 border-t-[1px]'></footer>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local';
 import './globals.css';
 import Nav from './_components/nav';
 import Toaster from '@/components/ui/sonner';
+import Footer from './_components/footer';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Toaster />
         <Nav />
         <main className='mt-[80px]'>{children}</main>
+        <Footer />
       </body>
     </html>
   );
