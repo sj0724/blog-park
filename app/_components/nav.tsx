1faf902db9c124038a2b4159950a38daa81e51cc
sj0724 <sj07245@naver.com>
2024-09-28 23:43:20 +0900

    fix: 내 포스트, userId값 param로 받음

--- app/_components/nav.tsx
+++ app/_components/nav.tsx
@@ -1,10 +1,10 @@
-import { auth } from '@/auth';
 import Link from 'next/link';
 import LogoutButton from './logout-button';
 import LinkCreatePost from './link-create-post';
+import { getSessionUserData } from '../data/user';
 
 export default async function Nav() {
-  const session = await auth();
+  const session = await getSessionUserData();
 
   return (
     <section className='fixed top-0 left-0 right-0 flex justify-between items-center px-4 py-2 h-20 bg-white/50 backdrop-blur-xl shadow-md z-40'>
@@ -14,7 +14,7 @@ export default async function Nav() {
         </Link>
         <div className='flex text-xl font-bold gap-2 justify-between'>
           <Link href='/post/list'>전체 포스트</Link>
-          {session && <Link href='/my/post'>내 포스트</Link>}
+          {session && <Link href={`/user/${session.id}`}>내 포스트</Link>}
         </div>
       </div>
       <div className='flex text-xl font-bold gap-2 justify-between'>
