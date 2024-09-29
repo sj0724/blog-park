390d921b3416a039927739bffb681e5a98589ced
sj0724 <sj07245@naver.com>
2024-09-30 00:35:06 +0900

    feat: 포스트 수정 기능 구현

--- app/(detail)/post/[postId]/_components/post-button-container.tsx
+++ app/(detail)/post/[postId]/_components/post-button-container.tsx
@@ -4,7 +4,7 @@ import PostDeleteButton from './post-delete-button';
 export default function PostButtonContainer({ postId }: { postId: string }) {
   return (
     <div className='flex gap-2'>
-      <Link href='/post/edit'>수정</Link>
+      <Link href={`/post/edit/${postId}`}>수정</Link>
       <PostDeleteButton postId={postId} />
     </div>
   );
--- app/(detail)/post/[postId]/_components/post-contents.tsx
+++ app/(detail)/post/[postId]/_components/post-contents.tsx
@@ -25,10 +25,12 @@ export default async function PostContents({
     <div className='w-[800px] flex flex-col gap-12'>
       <div className='flex flex-col gap-4'>
         <h1 className='text-6xl font-extrabold'>{title}</h1>
-        <div className='flex gap-2 items-center text-center h-10'>
-          <p className='font-semibold text-lg'>{user.name}</p>
-          <DotFilledIcon width={10} height={10} />
-          <p className='text-gray-600'>{formatData}</p>
+        <div className='flex justify-between'>
+          <div className='flex gap-2 items-center text-center h-10'>
+            <p className='font-semibold text-lg'>{user.name}</p>
+            <DotFilledIcon width={10} height={10} />
+            <p className='text-gray-600'>{formatData}</p>
+          </div>
           {currentUser?.id === user.id && (
             <PostButtonContainer postId={postId} />
           )}
