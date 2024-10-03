e269b3c019121e6d4c17fcb01fd0e2a1c5c57815
sj0724 <sj07245@naver.com>
2024-10-03 11:57:46 +0900

    fix: 좋아요, 팔로우 useState로 상태 관리

--- app/(detail)/user/[userId]/_components/like-container.tsx
+++ app/(detail)/user/[userId]/_components/like-container.tsx
@@ -6,7 +6,7 @@ export default async function LikeContainer() {
 
   return (
     <div className='flex flex-col gap-10'>
-      <h1 className='text-3xl font-bold'>좋아요 목록</h1>
+      <h1 className='text-2xl font-bold'>좋아요 목록</h1>
       <ul className='grid grid-cols-3 flex-col items-center'>
         {!likeList || likeList.length === 0 ? (
           <div className='flex items-center justify-center gap-5 border rounded-lg shadow-lg w-[550px] h-52 px-8 py-4 hover:-translate-y-1 transition-transform text-2xl font-semibold'>
