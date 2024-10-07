5f9529ec406601718b9330cbbe0f074105c01032
sj0724 <sj07245@naver.com>
2024-10-07 14:55:56 +0900

    fix: utc에서 ktc로 변경

--- utils/calculateData.ts
+++ utils/calculateData.ts
@@ -1,31 +1,31 @@
 const calculateDate = (createdAt: string) => {
   const nowDate = new Date();
-  const createdDate = new Date(createdAt);
-  const date = (Number(nowDate) - Number(createdDate)) / 1000;
-  if (date < 60 * 2) {
+  const utcDate = new Date(createdAt); // UTC 시간
+  const kstOffset = 9 * 60 * 60 * 1000; // KST offset (9시간)
+  const kst = new Date(utcDate.getTime() + kstOffset); // KST로 변환
+  const kstDate = (Number(nowDate) - Number(kst)) / 1000; // 초 단위로 변환
+
+  if (kstDate < 60 * 2) {
     return { time: 1, result: '분' };
   }
-  if (date <= 60 * 59) {
-    const minute = Math.floor(date / 59);
+  if (kstDate < 60 * 60) {
+    const minute = Math.floor(kstDate / 60);
     return { time: minute, result: '분' };
   }
-  if (date < 60 * 60 * 24) {
-    const day = Math.floor(date / 60 / 60);
-    return { time: day, result: '시간' };
+  if (kstDate < 60 * 60 * 24) {
+    const hour = Math.floor(kstDate / 3600);
+    return { time: hour, result: '시간' };
   }
-  if (date < 60 * 60 * 24 * 30) {
-    const day = Math.floor(date / 60 / 60 / 24);
+  if (kstDate < 60 * 60 * 24 * 30) {
+    const day = Math.floor(kstDate / 86400);
     return { time: day, result: '일' };
   }
-  if (date < 60 * 60 * 24 * 30 * 12) {
-    const month = Math.floor(date / 60 / 60 / 24 / 30);
+  if (kstDate < 60 * 60 * 24 * 30 * 12) {
+    const month = Math.floor(kstDate / 2592000);
     return { time: month, result: '달' };
   }
-  if (date >= 60 * 60 * 24 * 30 * 12) {
-    const year = Math.floor(date / 60 / 60 / 24 / 30 / 12);
-    return { time: year, result: '년' };
-  }
-  return { time: date, result: '분' };
+  const year = Math.floor(kstDate / 31536000);
+  return { time: year, result: '년' };
 };
 
 export default calculateDate;
