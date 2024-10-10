b9f8b34c6f5a0cb352a53535b62aa5f18dcecd5c
sj0724 <sj07245@naver.com>
2024-10-10 20:08:42 +0900

    feat: OAuth 컴포넌트 추가

---
+++ app/(auth)/sign-in/_components/oauth-form.tsx
@@ -0,0 +1,18 @@
+import { signIn } from '@/auth';
+import { Button } from '@/components/ui/button';
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
+
+export default function OAuthForm() {
+  return (
+    <form
+      action={async () => {
+        'use server';
+        await signIn('github');
+      }}
+    >
+      <Button type='submit'>
+        <GitHubLogoIcon width={30} height={30} />
+      </Button>
+    </form>
+  );
+}
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,12 @@
 import LoginForm from './_components/login-form';
+import OAuthForm from './_components/oauth-form';
 
 export default async function Page() {
   return (
     <div className='flex flex-col justify-center items-center h-[calc(100vh-52px)]'>
       <div className='w-96'>
         <LoginForm />
+        <OAuthForm />
       </div>
     </div>
   );
