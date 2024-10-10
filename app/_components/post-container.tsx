45614d28e7d7a31e01c87a7196463b2e69199fe2
sj0724 <sj07245@naver.com>
2024-10-10 20:10:00 +0900

    style: post스타일 수정

--- app/_components/post-container.tsx
+++ app/_components/post-container.tsx
@@ -10,19 +10,27 @@ export default async function PostContainer({ list, title }: Props) {
   return (
     <div className='flex flex-col gap-10'>
       <h1 className='text-2xl font-bold'>{title}</h1>
-      <ul className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 items-center justify-center'>
-        {list.map((post) => (
-          <li key={post.id} className='flex justify-center'>
-            <PostCard
-              id={post.id}
-              title={post.title}
-              summation={post.summation}
-              createdAt={post.createdAt}
-              owner={post.posts_user_id_fkey}
-            />
-          </li>
-        ))}
-      </ul>
+      {!list || list.length === 0 ? (
+        <ul className='flex flex-col items-center'>
+          <div className='flex items-center justify-center gap-5 border rounded-lg shadow-lg w-1/2 h-52 px-8 py-4 hover:-translate-y-1 transition-transform text-2xl font-semibold'>
+            작성한 포스트가 없습니다!
+          </div>
+        </ul>
+      ) : (
+        <ul className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 items-center justify-center'>
+          {list.map((post) => (
+            <li key={post.id} className='flex justify-center'>
+              <PostCard
+                id={post.id}
+                title={post.title}
+                summation={post.summation}
+                createdAt={post.createdAt}
+                owner={post.posts_user_id_fkey}
+              />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
