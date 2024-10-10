c9df9d49f9aad887e340d5e4a85a6857f794cb11
sj0724 <sj07245@naver.com>
2024-10-10 16:01:04 +0900

    fix: prisma adapter제거, prisma 제거

--- auth.ts
+++ auth.ts
@@ -1,10 +1,8 @@
 /* eslint-disable no-param-reassign */
 import NextAuth, { NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import { PrismaAdapter } from '@auth/prisma-adapter';
 import { verifyPassword } from './lib/utils';
 import { LoginSchema } from './schema';
-import db from './lib/db';
 import { supabase } from './utils/supabase';
 
 export const authConfig = {
@@ -16,23 +14,21 @@ export const authConfig = {
       },
       authorize: async (credentials) => {
         try {
-          const validate = LoginSchema.safeParse(credentials);
-          if (validate.success) {
-            const { email, password } = validate.data;
+          const validate = await LoginSchema.parseAsync(credentials);
+          const { email, password } = validate;
 
-            const { data: user } = await supabase
-              .from('users') // 테이블 이름
-              .select('*') // 모든 컬럼 선택
-              .eq('email', email)
-              .single(); // 단일 결과만 반환
+          const { data: user } = await supabase
+            .from('users') // 테이블 이름
+            .select('*') // 모든 컬럼 선택
+            .eq('email', email)
+            .single(); // 단일 결과만 반환
 
-            if (!user) return null;
+          if (!user) return null;
 
-            const passwordsMatch = verifyPassword(password, user.password);
+          const passwordsMatch = verifyPassword(password, user.password);
 
-            if (passwordsMatch) return user;
-          }
-          return null;
+          if (!passwordsMatch) return null;
+          return user;
         } catch {
           return null;
         }
@@ -59,7 +55,6 @@ export const {
   signOut,
   auth,
 } = NextAuth({
-  adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
   trustHost: true,
