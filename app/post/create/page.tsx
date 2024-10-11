5460e159da53bb106f8df553f5f3a0bfcd1c4f92
sj0724 <sj07245@naver.com>
2024-10-11 23:32:04 +0900

    fix: post 공통 에디터 구현

--- app/post/create/page.tsx
+++ app/post/create/page.tsx
@@ -1,9 +1,9 @@
-import CreatePostContainer from './_components/create-post-container';
+import PostEditor from '@/components/post-editor';
 
 export default function Page() {
   return (
     <div className='flex max-w-screen justify-center max-h-[calc(100vh-80px-80px)] h-screen'>
-      <CreatePostContainer />
+      <PostEditor />
     </div>
   );
 }
