20960eb44447b6a341619bb4d7cdd60957653545
sj0724 <sj07245@naver.com>
2024-10-11 23:54:55 +0900

    fix: 구글 로그인 버튼 아이콘 추가 및 로그인 폼 수정

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,17 +1,23 @@
 import Link from 'next/link';
 import LoginForm from './_components/login-form';
 import OAuthForm from './_components/oauth-form';
+import { Separator } from '@/components/ui/separator';
 
 export default async function Page() {
   return (
     <div className='flex flex-col justify-center items-center h-[calc(100vh-52px)]'>
       <div className='flex flex-col gap-3 max-w-96 w-full px-5'>
         <LoginForm />
+        <div className='flex items-center justify-center gap-3 w-full'>
+          <Separator className='w-2/5' />
+          <p className='text-gray-500'>or</p>
+          <Separator className='w-2/5' />
+        </div>
         <OAuthForm />
         <div className='flex justify-center gap-1 items-center'>
-          <p>회원이 아니라면</p>
+          <p className='text-sm'>회원이 아니라면</p>
           <Link href='/sign-up'>
-            <p className='text-lg w-fit h-fit font-bold'>가입하기!</p>
+            <p className='text-base w-fit h-fit font-bold'>가입하기!</p>
           </Link>
         </div>
       </div>
