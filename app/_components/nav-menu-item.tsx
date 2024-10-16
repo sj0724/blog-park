1e0f4bea6678674e5adeebdc6cb679f27ab756ba
sj0724 <sj07245@naver.com>
2024-10-16 10:32:15 +0900

    feat: 댓글 로직 fetch 적용 및 next cache 테스트

--- app/_components/nav-menu-item.tsx
+++ app/_components/nav-menu-item.tsx
@@ -1,4 +1,4 @@
-import { NavigationMenuLink } from '@/components/ui/navigation-menu';
+import Link from 'next/link';
 
 interface Props {
   title: string;
@@ -7,13 +7,13 @@ interface Props {
 
 export default function ListItem({ title, href }: Props) {
   return (
-    <NavigationMenuLink
+    <Link
       href={href}
       className='block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground'
     >
       <div className='font-medium leading-none text-nowrap text-center text-base'>
         {title}
       </div>
-    </NavigationMenuLink>
+    </Link>
   );
 }
