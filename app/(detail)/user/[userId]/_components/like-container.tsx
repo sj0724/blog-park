23fe23aae50f38f029389be2a2b5b35c928da135
sj0724 <sj07245@naver.com>
2024-10-18 00:28:28 +0900

    style: 좋아요 리스트 스타일 수정

--- app/(detail)/user/[userId]/_components/like-container.tsx
+++ app/(detail)/user/[userId]/_components/like-container.tsx
@@ -14,7 +14,7 @@ export default async function LikeContainer() {
           </div>
         </ul>
       ) : (
-        <ul className='grid grid-cols-3 w-full gap-4'>
+        <ul className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 items-center justify-center'>
           {likeList.map((post) => (
             <li key={post.id}>
               <PostCard
