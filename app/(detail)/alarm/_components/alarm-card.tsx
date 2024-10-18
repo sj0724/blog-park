a65a4b0978c6dffae60a515e7be1ff25187bf788
sj0724 <sj07245@naver.com>
2024-10-18 23:52:38 +0900

    style: 알람 카드 스타일 수정

--- app/(detail)/alarm/_components/alarm-card.tsx
+++ app/(detail)/alarm/_components/alarm-card.tsx
@@ -46,7 +46,7 @@ export default function AlarmCard({
   return (
     <Link href={link}>
       <div
-        className={` rounded-lg flex flex-col py-3 px-5 shadow hover:shadow-lg hover:-translate-y-1 transition-transform w-full ${
+        className={`relative rounded-lg flex flex-col py-3 px-5 shadow hover:shadow-lg hover:-translate-y-1 transition-transform w-full ${
           readAlarm ? 'bg-gray-300' : 'bg-white'
         }`}
       >
@@ -54,16 +54,15 @@ export default function AlarmCard({
           <div className='flex items-center gap-2 text-sm'>
             <UserAvatar image={user.image} size='sm' />
             <div className='flex items-center'>
-              <p className='font-semibold text-nowrap'>{user.name}</p>
-              <p className='text-nowrap'>님이</p>
+              <p className='text-wrap'>
+                <span className='font-semibold'>{user.name}</span>
+                {`님이 ${content}`}
+                <span className='text-nowrap text-xs text-gray-400 pl-1'>{`${calculateData.time}${calculateData.result}전`}</span>
+              </p>
             </div>
           </div>
-          <div className='flex items-center gap-1 text-sm w-full'>
-            <p className='text-nowrap'>{content}</p>
-            <p className='text-nowrap text-xs text-gray-400'>{`${calculateData.time}${calculateData.result}전`}</p>
-          </div>
         </div>
-        <div className='flex justify-end w-full'>
+        <div className='relative md:right-5 md:top-7 lg:right-5 lg:top-7 md:absolute lg:absolute flex justify-end md:w-fit lg:w-fit'>
           <div className='flex items-center gap-2'>
             <div onClick={handleRead} className='cursor-pointer'>
               {readAlarm ? <CircleCheckBig size={20} /> : <Circle size={20} />}
