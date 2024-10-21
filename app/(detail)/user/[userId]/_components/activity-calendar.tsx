c76b15bb47f689c388ce4f8ea868f0d7bd0e61d4
sj0724 <sj07245@naver.com>
2024-10-21 21:21:48 +0900

    fix: 기존 컨테이너 삭제 및 로그 위치에 따른 로그 정보 팝업 위치 스타일링

--- app/(detail)/user/[userId]/_components/activity-calendar.tsx
+++ app/(detail)/user/[userId]/_components/activity-calendar.tsx
@@ -1,89 +1,14 @@
-import {
-  formatDateTz,
-  getDateFromDay,
-  getDayInYear,
-  getFirstDay,
-} from '@/utils/formatData';
-import CalendarSingleDay from './calendar-single-day';
 import { getLogById } from '@/app/data/log';
-import { Log } from '@/type';
-
-const DAYS_KOREAN = ['일', '월', '화', '수', '목', '금', '토'];
-
-const MONTHS_KOREAN = [
-  '1월',
-  '2월',
-  '3월',
-  '4월',
-  '5월',
-  '6월',
-  '7월',
-  '8월',
-  '9월',
-  '10월',
-  '11월',
-  '12월',
-];
+import CalendarContainer from './calendar-container';
 
 export default async function ActivityCalendar({ userId }: { userId: string }) {
   const currentYear = new Date().getFullYear();
-  const firstDayIndex = getFirstDay(currentYear);
-  const totalDay = getDayInYear(currentYear);
   const log = await getLogById({ userId, year: currentYear });
-
-  // 전체 로그 배열을 날짜값을 키값으로 객체 생성
-  const logByDate: Record<string, Log[]> = log!.reduce((acc, entry) => {
-    const date = formatDateTz(entry.updated_at!);
-    acc[date] = acc[date] || [];
-    acc[date].push(entry);
-    return acc;
-  }, {} as Record<string, Log[]>);
 
   return (
     <div className='flex flex-col gap-4'>
       <p className='text-2xl font-semibold'>활동 내역</p>
-      <div className='flex flex-col gap-3 border p-3 rounded-lg max-w-[800px] overflow-x-auto'>
-        <div className='ml-8 flex gap-12'>
-          {MONTHS_KOREAN.map((item) => (
-            <div
-              key={item}
-              className='text-sm text-nowrap text-gray-400 font-semibold'
-            >
-              {item}
-            </div>
-          ))}
-        </div>
-        <div className='flex gap-5'>
-          <div className='flex flex-col'>
-            {DAYS_KOREAN.map((item) => (
-              <div key={item} className='text-sm text-gray-400 font-semibold'>
-                {item}
-              </div>
-            ))}
-          </div>
-          <div className='grid grid-flow-col grid-rows-7 gap-1 pr-3'>
-            {Array.from({ length: firstDayIndex }).map((_, index) => (
-              <div key={index} className='w-3 h-3' />
-            ))}
-            {Array.from({ length: totalDay }, (_, index) => {
-              // 위에서 만들어진 객체에서 같은 날짜에 있는 프로퍼티값을 props로 전달
-              const day = index + 1;
-              const dayByYear = getDateFromDay(currentYear, day); // 현재 연도에서 해당 일수의 날짜로 변환
-              const formatDate = formatDateTz(dayByYear); // 날짜를 시간 제외한 포멧으로 변경
-              const matchLog = logByDate[formatDate] || [];
-
-              return (
-                <CalendarSingleDay
-                  key={day}
-                  day={day}
-                  year={currentYear}
-                  log={matchLog}
-                />
-              );
-            })}
-          </div>
-        </div>
-      </div>
+      <CalendarContainer log={log ? log : []} currentYear={currentYear} />
     </div>
   );
 }
--- app/(detail)/user/[userId]/_components/calendar-single-day.tsx
+++ app/(detail)/user/[userId]/_components/calendar-single-day.tsx
@@ -2,23 +2,43 @@
 
 import { Log } from '@/type';
 import { formatDateRange, getDateFromDay } from '@/utils/formatData';
-import { useState } from 'react';
+import { MutableRefObject, useEffect, useRef, useState } from 'react';
 
 interface Props {
   day: number;
   year: number;
   log: Log[];
+  containerRef: MutableRefObject<HTMLDivElement | null>;
 }
 
-export default function CalendarSingleDay({ day, year, log }: Props) {
+type DirectionStyle = {
+  [key: string]: string;
+};
+
+const directionStyle: DirectionStyle = {
+  right: 'origin-top-left translate-x-4',
+  left: 'origin-top-right right-0 -translate-x-4',
+  top: 'top-0',
+  bottom: 'bottom-0',
+};
+
+export default function CalendarSingleDay({
+  day,
+  year,
+  log,
+  containerRef,
+}: Props) {
   const [isHover, setIsHover] = useState(false);
+  const [xDirection, setXDirection] = useState('right');
+  const [yDirection, setYDirection] = useState('top');
+  const infoRef = useRef<HTMLDivElement | null>(null);
   const transformDay = getDateFromDay(year, day);
   const formatDate = formatDateRange({ dateString: transformDay });
   const logColor = (rate: number) => {
     if (50 > rate && rate !== 0) {
       return 'bg-blue-100 border-blue-100';
     }
-    if (rate >= 50 && rate < 100) {
+    if (rate >= 50 && rate < 150) {
       return 'bg-blue-300 border-blue-300';
     }
     if (rate >= 100) {
@@ -27,21 +47,39 @@ export default function CalendarSingleDay({ day, year, log }: Props) {
     return 'bg-none border';
   };
 
+  useEffect(() => {
+    if (containerRef.current && infoRef.current) {
+      const child = infoRef.current.getBoundingClientRect(); //로그 기록 위치
+      const parent = containerRef.current.getBoundingClientRect(); // 컨테이너 위치
+      const leftPosition = child.left - parent.left; // 컨테이너 기준 로그 태그 좌측 포지션
+      const topPosition = child.top - parent.top; // 컨테이너 기준 로그 태그 상단 포지션
+      if (leftPosition > parent.width - 130) {
+        // 컨테이너 넓이 - 로그 태그 넓이
+        setXDirection('left');
+      }
+      if (topPosition > parent.height - 110) {
+        // 컨테이너 높이 - 로그 태그 높이
+        setYDirection('bottom');
+      }
+    }
+  }, [containerRef]);
+
   return (
-    <div
-      className='relative'
-      onMouseEnter={() => setIsHover(true)}
-      onMouseLeave={() => setIsHover(false)}
-    >
+    <div className='relative'>
       <div
         className={`w-3 h-3 border rounded cursor-pointer ${
           log[0] && logColor(log[0].rate!)
         }`}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
       />
       <div
-        className={`${
-          isHover ? 'flex scale-100' : 'scale-0'
-        } absolute top-0 origin-top-left z-10 bg-white shadow-md flex-col w-32 h-fit p-2 gap-1 rounded-md transition-transform duration-900 translate-x-4`}
+        ref={infoRef}
+        className={`${isHover ? 'flex scale-100' : 'scale-0'} absolute z-10 ${
+          directionStyle[xDirection]
+        } ${
+          directionStyle[yDirection]
+        } bg-white shadow-md flex-col w-32 h-fit p-2 gap-1 rounded-md transition-transform duration-900`}
       >
         <p className='text-sm font-semibold text-nowrap'>{formatDate}</p>
         <div className='text-sm font-semibold text-gray-600'>
