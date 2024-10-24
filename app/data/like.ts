c7528f7279eb605981f0f750ee0d5972ae0e862e
sj0724 <sj07245@naver.com>
2024-10-24 10:42:27 +0900

    fix: 전체 좋아요 로직 제거

--- app/data/like.ts
+++ app/data/like.ts
@@ -1,22 +1,6 @@
 import { supabase } from '@/utils/supabase';
 import { getSessionUserData } from './user';
 
-export const getLikeById = async (postId: string) => {
-  const result = await supabase
-    .from('likes')
-    .select(
-      `*,users: likes_user_id_fkey (
-        id,
-        name,
-        email
-      )`,
-      { count: 'exact' }
-    )
-    .eq('post_id', postId);
-
-  return result;
-};
-
 export const getMyLikeByPostId = async ({ postId }: { postId: string }) => {
   const session = await getSessionUserData();
   if (!session) return false;
