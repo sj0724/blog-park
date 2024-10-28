60204a9dc23e970a6fdd0949994caf2ebe91fac8
sj0724 <sj07245@naver.com>
2024-10-28 17:30:56 +0900

    feat: 데이터 타입 지정 및 로그 연동 로직 적용

--- app/(detail)/user/[userId]/_components/repo-list.tsx
+++ app/(detail)/user/[userId]/_components/repo-list.tsx
@@ -1,17 +1,34 @@
 'use client';
 
+import { addGithubLog } from '@/app/action/log';
+import { getPrByRepo } from '@/app/data/log';
 import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 import { ChangeEvent, useState } from 'react';
+import { toast } from 'sonner';
 
 export default function RepoList({ list }: { list: string[] }) {
   const [repo, setRepo] = useState('');
+  const router = useRouter();
 
   const handleCheckbox = (e: ChangeEvent<HTMLInputElement>) => {
     setRepo(e.target.value);
   };
 
-  const connectRepo = () => {
-    console.log(repo);
+  const connectRepo = async () => {
+    const prList = await getPrByRepo(repo);
+    const objectToArr = Object.keys(prList).map((date) => ({
+      createdAt: date,
+      count: prList[date].count,
+      url: prList[date].url,
+    }));
+    const result = await addGithubLog(objectToArr);
+    if (result.success) {
+      toast.message(result.message);
+      router.refresh();
+    } else {
+      toast.error(result.message);
+    }
   };
 
   return (
