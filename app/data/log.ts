7972739987e5c1f881048268ade41588321c5866
sj0724 <sj07245@naver.com>
2024-10-29 17:40:32 +0900

    fix: 레포 pr에서 커밋으로 변경

--- app/data/log.ts
+++ app/data/log.ts
@@ -46,13 +46,13 @@ export const getAllRepo = async (page = 1) => {
   return repoList;
 };
 
-export const getPrByRepo = async (repo: string) => {
+export const getCommitByRepo = async (repo: string) => {
   const user = await getSessionUserData();
-  const prList = [];
+  const commitList = [];
   let page = 1;
 
   while (true) {
-    const url = `https://api.github.com/repos/${user?.name}/${repo}/pulls?state=all&page=${page}&per_page=100&sort=created`; // 모든 PR 가져오기
+    const url = `https://api.github.com/repos/${user?.name}/${repo}/commits?author=${user?.name}&page=${page}&per_page=100`; // 모든 PR 가져오기
     const response = await fetch(url, { headers });
 
     if (!response.ok) {
@@ -63,15 +63,15 @@ export const getPrByRepo = async (repo: string) => {
     if (data.length === 0) {
       break; // 데이터가 없으면 종료
     }
-    prList.push(...data);
+    commitList.push(...data);
     page++;
   }
 
-  const prByDate = prList.reduce((acc, pr) => {
-    const date = new Date(pr.created_at).toISOString().split('T')[0]; // YYYY-MM-DD 형식으로 날짜만 추출
+  const commitByDate = commitList.reduce((acc, log) => {
+    const date = new Date(log.commit.author.date).toISOString().split('T')[0]; // YYYY-MM-DD 형식으로 날짜만 추출
     if (!acc[date]) {
-      // 해당 날짜의 첫 번째 PR이면 URL 포함하여 초기화
-      acc[date] = { count: 1, url: pr.html_url };
+      // 해당 날짜의 첫 번째 커밋이면 count 설정
+      acc[date] = { count: 1 };
     } else {
       // 이미 날짜가 있으면 count만 증가
       acc[date].count += 1;
@@ -79,5 +79,5 @@ export const getPrByRepo = async (repo: string) => {
     return acc;
   }, {});
 
-  return prByDate;
+  return commitByDate;
 };
