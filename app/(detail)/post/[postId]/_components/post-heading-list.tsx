93f15b9df33f3ab1b7973a83050e113720013334
sj0724 <sj07245@naver.com>
2024-11-26 22:59:54 +0900

    fix: 옵저버에 스크롤 방향에 따른 액션 구분

--- app/(detail)/post/[postId]/_components/post-heading-list.tsx
+++ app/(detail)/post/[postId]/_components/post-heading-list.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 'use client';
 
 import { useEffect, useState } from 'react';
@@ -8,14 +9,17 @@ interface Props {
 }
 
 export default function PostHeadingList({ content }: Props) {
-  const [activeHeading, setActiveHeading] = useState('');
+  const [activeHeading, setActiveHeading] = useState(0);
   const headingList = content.match(/^(#{1,3}\s.*)$/gm) || [];
+  const firstHeading = headingList[0]
+    ? headingList[0].replace(/#/g, '').trim()
+    : '';
 
   useEffect(() => {
-    const navElement = document.querySelector('nav');
     const headings = document.querySelectorAll('h1, h2, h3');
     const observedSet = new Set(); // 감지된 요소만 저장하는 데이터
-    if (!navElement || !headings) return;
+    let lastScrollTop = 0; // 이전 스크롤 위치
+    if (!headings) return;
 
     const observerCallback = (entries: IntersectionObserverEntry[]) => {
       entries.forEach((entry) => {
@@ -23,9 +27,20 @@ export default function PostHeadingList({ content }: Props) {
           observedSet.add(entry.target);
           return;
         }
-        if (!entry.isIntersecting) {
-          setActiveHeading(() => entry.target.id);
+        const currentScrollTop =
+          window.scrollY || document.documentElement.scrollTop;
+        if (currentScrollTop > lastScrollTop) {
+          if (!entry.isIntersecting) {
+            if (entry.target.id === firstHeading) return;
+            setActiveHeading((prev) => prev + 1);
+          }
+        } else if (currentScrollTop < lastScrollTop) {
+          if (entry.isIntersecting) {
+            if (entry.target.id === firstHeading) return;
+            setActiveHeading((prev) => prev - 1);
+          }
         }
+        lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
       });
     };
 
@@ -51,7 +66,7 @@ export default function PostHeadingList({ content }: Props) {
     return () => {
       headings.forEach((heading) => observer.unobserve(heading));
     };
-  }, []);
+  }, [firstHeading]);
 
   return (
     <div className='hidden fixed top-40 left-1/2 translate-x-[420px] h-fit lg:flex w-60'>
@@ -62,7 +77,7 @@ export default function PostHeadingList({ content }: Props) {
             <li key={index} className='w-fit'>
               <Heading
                 text={item}
-                isActive={activeHeading === headingId}
+                isActive={activeHeading === index}
                 id={headingId}
               />
             </li>
