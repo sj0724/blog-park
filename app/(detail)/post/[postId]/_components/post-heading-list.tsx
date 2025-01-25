cadcc7b7288b3f61a24e3b7c33d5232944a8dbb9
sj0724 <sj07245@naver.com>
2025-01-25 23:07:08 +0900

    fix: 헤딩 리스트 스크롤 추가

--- app/(detail)/post/[postId]/_components/post-heading-list.tsx
+++ app/(detail)/post/[postId]/_components/post-heading-list.tsx
@@ -11,7 +11,7 @@ export default function PostHeadingList({ content }: Props) {
   const headingList = content.match(/^(#{1,3}\s.*)$/gm) || [];
 
   return (
-    <div className='hidden fixed top-40 left-1/2 translate-x-[420px] h-fit xl:flex w-60'>
+    <div className='hidden fixed top-40 left-1/2 translate-x-[420px] h-fit max-h-[400px] xl:flex w-64 overflow-y-scroll'>
       <ul className='flex flex-col gap-0.5'>
         {headingList.map((item, index) => {
           const headingId = item.replace(/#/g, '').trim();
